3f0d8f3501d68fca46d18ac340e080e41cd801a8
Evgeny <shparaga.evgeny@yandex.ru>
2020-02-21 20:46:32 +0300

    Cata_1_Completed

--- Cata8_001.js
+++ Cata8_001.js
@@ -1,1 +1,11 @@
-aler("Hello, World");
+let arr = [1, 3, 5, -5, -2, 12];
+let x = arr.length;
+let result = 0;
+
+for(let i = 0; i < x; i++){
+    if(arr[i] > 0){
+        result += arr[i];
+    }
+}
+
+console.log(result);
